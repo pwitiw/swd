ca0c220b9eb2de223cc1e765c413583ffae55430
marta <mar.ta.ko@interia.pl>
2016-05-03 20:03:52 +0200

    gui - dynamiczne dodawanie pól do adresów, przycisk tymczasowy 'czytaj'

--- project/app/src/main/java/pwr/swd/MainActivity.java
+++ project/app/src/main/java/pwr/swd/MainActivity.java
@@ -2,12 +2,50 @@ package pwr.swd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout mLayout;
+    private Button mButton;
+    private ArrayList<EditText> editTexts = new ArrayList<EditText>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = (LinearLayout) findViewById(R.id.linearlayout);
+        mButton = (Button) findViewById(R.id.btnAdd);
+        Button mButton2 = (Button) findViewById(R.id.btnGetAdress);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText editText2 = createNewEditText();
+                mLayout.addView(editText2);
+                editTexts.add(editText2);
+
+            }
+        });
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                for(EditText e: editTexts )
+                    Log.v("EditText", e.getText().toString());
+            }
+        });
+
+    }
+    private EditText createNewEditText() {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        final EditText editText = new MyEditText(this);
+        editText.setLayoutParams(lparams);
+
+        return editText;
     }
 }
