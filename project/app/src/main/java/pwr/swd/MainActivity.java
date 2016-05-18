fb00817a2756feca74f8ac88b31d62c21f96c387
Patryk <witiw.p@gmail.com>
2016-05-18 18:10:24 +0200

    few changes

--- project/app/src/main/java/pwr/swd/MainActivity.java
+++ project/app/src/main/java/pwr/swd/MainActivity.java
@@ -1,7 +1,15 @@
 package pwr.swd;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 import pwr.swd.services.RestService;
 import pwr.swd.services.RouteService;
@@ -9,19 +17,59 @@ import pwr.swd.utils.Consts;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyRecyclerAdapter adapter;
 
+    private ArrayList<Record> dataSet = new ArrayList<>();
     private RouteService routeService;
 
-    private RestService restService;
 
+    private RestService restService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        routeService = new RouteService();
-        restService = new RestService();
 
-        restService.getDataForLocalizations(Consts.LOCALES);
+       // requestWindowFeature(Window.FEATURE_NO_TITLE);
+       // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        routeService = new RouteService();
+        setUpButtons();
+        restService = new RestService();
+        setUpRecyclerView();
+
     }
 
+    private void setUpButtons() {
+        Button bt = (Button)findViewById(R.id.add_new);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.addNewItem(new Record());
+            }
+        });
+
+        Button bt2 = (Button)findViewById(R.id.find_fastest);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (Record item:
+                        dataSet) {
+                    Log.v("Records", item.toString());
+                }
+            }
+        });
+
+    }
+
+    private void setUpRecyclerView() {
+
+        adapter = new MyRecyclerAdapter();
+        adapter.setDataSet(this.dataSet);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+    }
 }
---
+++ project/app/src/main/java/pwr/swd/MyRecyclerAdapter.java
@@ -0,0 +1,113 @@
+package pwr.swd;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+import static android.support.v7.widget.RecyclerView.Adapter;
+import static android.support.v7.widget.RecyclerView.ViewHolder;
+
+public class MyRecyclerAdapter extends Adapter {
+
+    public ArrayList<Record> dataSet;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_list_item, null);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (holder instanceof MyViewHolder) {
+            ((MyViewHolder)holder).mapValues(dataSet.get(position), position);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+
+    public void addNewItem(Record item) {
+        dataSet.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void setDataSet(ArrayList<Record> dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    class MyViewHolder extends ViewHolder {
+
+        public EditText address;
+        public EditText time;
+        public int position;
+
+        private boolean dontChangeThisShit;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            dontChangeThisShit = true;
+            address = (EditText)itemView.findViewById(R.id.address);
+            time = (EditText)itemView.findViewById(R.id.textClock);
+
+            address.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if(dontChangeThisShit)
+                        return;
+                    if(position <= dataSet.size()) {
+                        dataSet.get(position).address = s.toString();
+                    }
+                }
+            });
+            time.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if(dontChangeThisShit)
+                        return;
+                    if(position <= dataSet.size()) {
+                        dataSet.get(position).time = s.toString();
+                    }
+                }
+            });
+        }
+
+        public void mapValues(Record record, int position){
+            dontChangeThisShit = true;
+
+            address.setText(record.address);
+            time.setText(record.time);
+            this.position = position;
+
+            dontChangeThisShit = false;
+        }
+    }
+}
